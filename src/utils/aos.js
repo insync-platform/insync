edbf7fa1f3199f6539efe67b1f270e7877b21163
PatrickHuizinga88 <patrickhuizinga44@gmail.com>
2026-06-15 00:20:04 +0200

    Animations and styling changes

--- src/utils/aos.js
+++ src/utils/aos.js
@@ -1,3 +1,6 @@
-import AOS from 'aos';
-import 'aos/dist/aos.css'
-AOS.init()
+import AOS from "aos";
+import "aos/dist/aos.css";
+
+AOS.init({
+  disable: () => window.matchMedia("(prefers-reduced-motion: reduce)").matches,
+});
